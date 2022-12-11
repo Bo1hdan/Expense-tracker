e20762d96409e03df2f4697f95b4c9a8beecdf40
Bo1hdan <87267253+Bo1hdan@users.noreply.github.com>
2022-12-11 12:43:21 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
+const expenseType = document.querySelector("#type");
 const expenseName = document.querySelector("#name");
 const expenseDate = document.querySelector("#date");
 const expenseAamount = document.querySelector("#amount");
 const addExpense = document.querySelector("#addExpense");
 const form = document.querySelector("#expenseForm");
+const tableHead= document.querySelector("#table-head");
 const tableBody = document.querySelector("#table-body");
+const tableFooter =document.querySelector("#table-footer");
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const newExName = expenseName.value;
   const newExDate = expenseDate.value;
   const newExAmount = expenseAamount.value;
+  const newType = expenseType.value;
 
   const row = tableBody.insertRow();
+  const cell0 = row.insertCell();
   const cell1 = row.insertCell();
   const cell2 = row.insertCell();
   const cell3 = row.insertCell();
+  cell0.innerHTML = newType;
   cell1.innerHTML = newExName;
   cell2.innerHTML = newExDate;
   cell3.innerHTML = newExAmount;
+  expenseType.value = "";
   expenseName.value = "";
   expenseDate.value = "";
   expenseAamount.value = "";
-  // newName.value = newExName;
-  // newDate.value = newExDate;
-  // newAmount.value = newExAmount;
-  // const newRow = document.createElement('tr');
-  // const newName = document.createElement('td');
-  // newRow.append(newName.value=newExName);
-  // const newDate = document.createElement('td');
-  // newRow.append(newDate.innerText=newExDate);
-  // const newAmount = document.createElement('td');
-  // newRow.append(newAmount.innerHTML=newExAmount);
+ 
 });
